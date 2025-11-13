373cc76cd8fe6399d0c215e41bfa7f328be5bbf2
Nick Landry <nglandry@mines.edu>
2025-11-13 15:54:25 -0700

    made changes to queries

--- db_util.py
+++ db_util.py
@@ -16,15 +16,14 @@ CPU Model: Apple M2 8 Core (4 performance, 4 efficiency) CPU
 Memory: 16 GB LPDDR5 RAM
 OS: Mac OS Tahoe 26.1
 Database Version: DuckDB v1.4.1 (Andium) b390a7c376
-TODO: Check Activity Monitory before running tests to ensure relative consistency, kill all bad background tasks
 
 Data Generation:
-TPC-H with scale factor 1 and/or 10
+TPC-H with scale factor 100
 '''
 
 # Creating a duckdb file using tpch
-DB_FILE = os.path.join('db_files', 'tpch_sf10.duckdb')
-SCALE_FACTOR = 10
+DB_FILE = os.path.join('db_files', 'tpch_sf100.duckdb')
+SCALE_FACTOR = 100
 
 def create_db_files():
     # Create a file if it does not exist
@@ -44,12 +43,13 @@ def execute_test(num_joins_query, num_threads, memory_limit):
         'threads': num_threads,
         'memory_limit': memory_limit
     }
-    con = duckdb.connect(DB_FILE, read_only=True, config=run_config)
+    con = duckdb.connect(database=DB_FILE, read_only=True, config=run_config)
     for i in range(0, 13):
         # run query, get results
         start_time = time.time()
-        con.sql(num_joins_query).fetchall()
+        result = con.sql(num_joins_query).fetchall()
         end_time = time.time()
+        print(result)
         elapsed_time = end_time - start_time
         if i > 2:
             results.append(elapsed_time)
@@ -71,49 +71,6 @@ def save_result(threads, memory, joins, results):
         writer = csv.writer(file)
         row_arr = [threads, memory, joins, mean, std_dev, mad] + results
         writer.writerow(row_arr)
-
-# def run_query():
-#     query_to_run = """
-#     SELECT l_orderkey, o_orderdate, o_shippriority
-#     FROM customer
-#     JOIN orders ON c_custkey = o_custkey
-#     JOIN lineitem ON l_orderkey = o_orderkey
-#     WHERE c_mktsegment = 'AUTOMOBILE'
-#     LIMIT 100;
-#     """
-
-#     run_config = {
-#     'threads': 4,
-#     'memory_limit': '1GB'
-#     }
-
-#     try:
-#         con = duckdb.connect(DB_FILE, read_only=True, config=run_config)
-
-#         # --- This is the timing for one run ---
-#         start_time = time.time()
-        
-#         # Execute the query. .fetchall() ensures the query is fully processed.
-#         con.sql(query_to_run).fetchall()
-        
-#         end_time = time.time()
-#         # --- End of timing ---
-        
-#         elapsed_time = end_time - start_time
-        
-#         print(f"Query executed successfully.")
-#         print(f"Wall-clock time: {elapsed_time:.4f} seconds")
-
-#         # In your full script, you would save this 'elapsed_time'
-#         # results_list.append(elapsed_time)
-        
-#     except duckdb.Error as e:
-#         print(f"An error occurred: {e}")
-
-#     finally:
-#         # Always close the connection
-#         if 'con' in locals():
-#             con.close()
 
 def run_tests():
     join_1_query = """
@@ -148,7 +105,7 @@ def run_tests():
     JOIN supplier ON l_suppkey = s_suppkey
     JOIN nation ON s_nationkey = n_nationkey
     JOIN region ON n_regionkey = r_regionkey
-    WHERE c_mktsegment = 'BUILDING' AND r_name = 'AMERICA';
+    WHERE c_mktsegment = 'BUILDING';
     """
 
     join_10_query = """
@@ -164,13 +121,13 @@ def run_tests():
     JOIN lineitem l2 ON l1.l_orderkey = l2.l_orderkey AND l1.l_partkey = l2.l_partkey
     JOIN lineitem l3 ON l2.l_orderkey = l3.l_orderkey AND l2.l_suppkey = l3.l_suppkey
     JOIN lineitem l4 ON l3.l_orderkey = l4.l_orderkey
-    WHERE c.c_mktsegment = 'FURNITURE' AND r.r_name = 'ASIA';
+    WHERE c.c_mktsegment = 'BUILDING';
     """
 
     queries = [join_1_query, join_2_query, join_3_query, join_5_query, join_10_query]
     query_join_amounts = [1, 2, 3, 5, 10]
-    num_threads = [1, 2, 4, 6, 8, 10, 12, 14, 16]
-    num_memory = ['1GB', '2GB', '4GB', '6GB', '8GB', '10GB', '12GB', '14GB', '16GB']
+    num_threads = [4, 6, 8, 10, 12, 14]
+    num_memory = ['4GB', '6GB', '8GB', '10GB', '12GB', '14GB']
 
     for thread in num_threads:
         print(f'Tests with {thread} threads:')
