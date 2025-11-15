aa8c58e9868f7a9059f59674676731ca24f751e4
Nicholas Landry <nglandry@mines.edu>
2025-11-15 07:20:01 -0700

    have good results for threads and memory

--- db_util.py
+++ db_util.py
@@ -18,7 +18,7 @@ OS: Mac OS Tahoe 26.1
 Database Version: DuckDB v1.4.1 (Andium) b390a7c376
 
 Data Generation:
-TPC-H with scale factor 10
+TPC-H with scale factor 100
 '''
 
 # Creating a duckdb file using tpch
@@ -44,8 +44,11 @@ def execute_test(num_joins_query, num_threads, memory_limit):
     #     'memory_limit': memory_limit
     # }
     con = duckdb.connect(database=DB_FILE, read_only=True)
-    con.execute(f"PRAGMA threads={num_threads};")
-    con.execute(f"PRAGMA memory_limit='{memory_limit}';")
+    if num_threads != None:
+        con.execute(f"PRAGMA threads={num_threads};")
+    if memory_limit != None:
+        con.execute(f"PRAGMA memory_limit='{memory_limit}';")
+
     for i in range(0, 13):
         # run query, get results
         start_time = time.time()
@@ -76,71 +79,81 @@ def save_result(threads, memory, joins, results):
 def run_tests():
     join_1_query = """
     SELECT COUNT(*)
-    FROM customer
-    JOIN orders ON c_custkey = o_custkey
-    WHERE c_mktsegment = 'BUILDING';
+    FROM customer c
+    JOIN nation n ON c.c_nationkey = n.n_nationkey
     """
 
     join_2_query = """
     SELECT COUNT(*)
-    FROM customer
-    JOIN orders ON c_custkey = o_custkey
-    JOIN lineitem ON o_orderkey = l_orderkey
-    WHERE c_mktsegment = 'BUILDING';
+    FROM customer c
+    JOIN nation n ON c.c_nationkey = n.n_nationkey
+    JOIN orders o ON c.c_custkey = o.o_custkey;
     """
 
     join_3_query = """
     SELECT COUNT(*)
-    FROM customer
-    JOIN orders ON c_custkey = o_custkey
-    JOIN lineitem ON o_orderkey = l_orderkey
-    JOIN supplier ON l_suppkey = s_suppkey
-    WHERE c_mktsegment = 'BUILDING';
+    FROM customer c
+    JOIN nation n ON c.c_nationkey = n.n_nationkey
+    JOIN orders o ON c.c_custkey = o.o_custkey
+    JOIN lineitem l ON o.o_orderkey = l.l_orderkey
     """
 
     join_5_query = """
     SELECT COUNT(*)
-    FROM customer
-    JOIN orders ON c_custkey = o_custkey
-    JOIN lineitem ON o_orderkey = l_orderkey
-    JOIN supplier ON l_suppkey = s_suppkey
-    JOIN nation ON s_nationkey = n_nationkey
-    JOIN region ON n_regionkey = r_regionkey
-    WHERE c_mktsegment = 'BUILDING';
+    FROM customer c
+    JOIN nation n ON c.c_nationkey = n.n_nationkey
+    JOIN orders o ON c.c_custkey = o.o_custkey
+    JOIN lineitem l ON o.o_orderkey = l.l_orderkey
+    JOIN part p ON l.l_partkey = p.p_partkey
+    JOIN partsupp ps 
+    ON p.p_partkey = ps.ps_partkey
+    AND l.l_suppkey = ps.ps_suppkey;
     """
 
     join_10_query = """
     SELECT COUNT(*)
     FROM region r
-    JOIN nation n ON r.r_regionkey = n.n_regionkey
-    JOIN customer c ON n.n_nationkey = c.c_nationkey
+    JOIN nation n1 ON r.r_regionkey = n1.n_regionkey
+    JOIN nation n2 ON n1.n_regionkey = n2.n_regionkey
+    JOIN customer c ON n2.n_nationkey = c.c_nationkey
     JOIN orders o ON c.c_custkey = o.o_custkey
-    JOIN lineitem l1 ON o.o_orderkey = l1.l_orderkey
-    JOIN supplier s ON s.s_suppkey = l1.l_suppkey
-    JOIN part p ON p.p_partkey = l1.l_partkey
-    JOIN partsupp ps ON p.p_partkey = ps.ps_partkey AND s.s_suppkey = ps.ps_suppkey
-    JOIN lineitem l2 ON l1.l_orderkey = l2.l_orderkey AND l1.l_partkey = l2.l_partkey
-    JOIN lineitem l3 ON l2.l_orderkey = l3.l_orderkey AND l2.l_suppkey = l3.l_suppkey
-    JOIN lineitem l4 ON l3.l_orderkey = l4.l_orderkey
-    WHERE c.c_mktsegment = 'BUILDING';
+    JOIN lineitem l ON o.o_orderkey = l.l_orderkey
+    JOIN supplier s ON l.l_suppkey = s.s_suppkey
+    JOIN nation n3 ON s.s_nationkey = n3.n_nationkey
+    JOIN part p ON l.l_partkey = p.p_partkey
+    JOIN partsupp ps 
+    ON p.p_partkey = ps.ps_partkey
+    AND s.s_suppkey = ps.ps_suppkey;
     """
 
     queries = [join_1_query, join_2_query, join_3_query, join_5_query, join_10_query]
     query_join_amounts = [1, 2, 3, 5, 10]
-    num_threads = [4, 6, 8, 10, 12]
+    num_threads = [1, 2, 4, 8, 16]
     # num_memory = ['128MB', '256MB', '512MB', '1GB', '2GB', '3GB']
-    num_memory = ['4GB', '5GB', '6GB', '7GB', '8GB']
+    num_memory = ['1GB', '2GB', '4GB', '8GB', '16GB']
 
+    # for thread in num_threads:
+    #     print(f'Tests with {thread} threads:')
+    #     for i in range(len(queries)):
+    #         results = execute_test(queries[i], thread, None)
+    #         save_result(thread, 'N/A', query_join_amounts[i], results)
+    #         print(f'Results for {query_join_amounts[i]} join(s): {results}')
+    
+    for memory in num_memory:
+        print(f'Tests with {memory} memory:')
+        for i in range(len(queries)):
+            results = execute_test(queries[i], None, memory)
+            save_result('N/A', memory, query_join_amounts[i], results)
+            print(f'Results for {query_join_amounts[i]} join(s): {results}')
 
-
-    for thread in num_threads:
-        print(f'Tests with {thread} threads:')
-        for memory in num_memory:
-            print(f'Tests with {memory} of memory')
-            for i in range(len(queries)):
-                results = execute_test(queries[i], thread, memory)
-                save_result(thread, memory, query_join_amounts[i], results)
-                print(f'Results for {query_join_amounts[i]}: {results}')
+    # for thread in num_threads:
+    #     print(f'Tests with {thread} threads:')
+    #     for memory in num_memory:
+    #         print(f'Tests with {memory} of memory')
+    #         for i in range(len(queries)):
+    #             results = execute_test(queries[i], thread, memory)
+    #             save_result(thread, memory, query_join_amounts[i], results)
+    #             print(f'Results for {query_join_amounts[i]}: {results}')
 
 create_db_files()
 run_tests()
